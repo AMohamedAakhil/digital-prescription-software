179f3991374dabf803c5a35aa05d2325b3bba9e5
Mahas1 <mahasvan@gmail.com>
2022-10-10 21:46:26 +0530

    WIP - implement API part 3

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ import api
 
 instance = api.RxNorm()
 
-result = instance.get_drug_list("ibuprofen").get("drugGroup").get("conceptGroup")
+count = 1
+continue_loop = True
 
-instance.parse_result(result)
+
+def confirm_continue():
+    global continue_loop
+    if input("Enter more? (y/n)").lower().strip() == "n":
+        continue_loop = False
+
+
+while continue_loop:
+    print("Drug Count", count)
+    drug_details = instance.get_required_drug()
+    if not drug_details:
+        confirm_continue()
+        break
+
+    other_info = instance.get_other_info()
+    # todo: enter this in a database or a json object?
+
+instance.parse_result(drug_details)
