5e71a6a17d207a545f0ba5cd4ad7ab52f8400641
Mahas1 <mahasvan@gmail.com>
2022-10-10 21:36:37 +0530

    WIP - implement API part 2

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import json
 
 import requests
 
+from assets import shell_assets
 
 class RxNorm:
 
@@ -28,8 +29,55 @@ class RxNorm:
                     name = property.get("name")
                     synonym = property.get("synonym")
                     type = self.tty_dict.get(property.get("tty"), "Generic Product")
-                    print(f"RXCUI: {rxcui}")
-                    print(f"Name: {name}")
-                    print(f"Synonym: {synonym}")
-                    print(f"Type: {type}")
+                    print(f"RXCUI: {shell_assets.colour_yellow(rxcui)}")
+                    print(f"Name: {shell_assets.colour_blue(name)}")
+                    if synonym:
+                        print(f"Synonym: {synonym}")
+                    print(f"Type: {shell_assets.colour_cyan(type)}")
                     print()
+
+    def fetch_drug_from_rxcui(self, concept_group: list, rxcui: str):
+        for entry in concept_group:
+            if entry.get("conceptProperties"):
+                for property in entry.get("conceptProperties"):
+                    fetched_rxcui = property.get("rxcui")
+                    name = property.get("name")
+                    synonym = property.get("synonym")
+                    type = self.tty_dict.get(property.get("tty"), "Generic Product")
+                    if fetched_rxcui == rxcui:
+                        return {
+                            "rxcui": fetched_rxcui,
+                            "name": name,
+                            "synonym": synonym,
+                            "type": type
+                        }
+        return {}
+
+    def get_required_drug(self):
+        name = input("Enter drug name: ")
+        results = self.get_drug_list(name).get("drugGroup").get("conceptGroup")
+        if results:
+            self.parse_result(results)
+            rxcui = input("\nPaste the RXCUI of the required drug: ")
+            if not rxcui.isdigit():
+                print(shell_assets.colour_red("Invalid RXCUI"))
+                return {}
+            drug = self.fetch_drug_from_rxcui(results, rxcui)
+            return drug
+        else:
+            print(shell_assets.colour_red("No results found"))
+            return {}
+
+    def get_other_info(self):
+        dosage = input("Enter dosage (Morn,Afn,Eve,Ngt): ".split(","))
+        duration = input("Enter duration (in days): ")
+        route = input("Enter administration route: ")
+        frequency = input("Enter frequency: ")
+        additional_info = input("Enter additional info (enter to skip): ")
+        return {
+            "dosage": dosage,
+            "duration": duration,
+            "route": route,
+            "frequency": frequency,
+            "additional_info": additional_info
+        }
--- main.py
+++ main.py
@@ -7,4 +7,3 @@ instance = api.RxNorm()
 result = instance.get_drug_list("ibuprofen").get("drugGroup").get("conceptGroup")
 
 instance.parse_result(result)
-
