9971745f37069358dabfd589415c7a0c792552f2
BootesVoid <78485654+AMohamedAakhil@users.noreply.github.com>
2022-10-11 23:30:45 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import api
 
+
+
 instance = api.RxNorm()
 
 root = Tk()
@@ -8,7 +10,7 @@ root.title('Digital Prescription Software')
 root.geometry('600x600')
 
 
-search_box = Entry(root, width=40, borderwidth=20, relief=SUNKEN)
+search_box = Entry(root, width=40, borderwidth=10, relief=SUNKEN)
 search_box.grid(row=0, column=0)
 
 
@@ -20,6 +22,7 @@ def Search_Click():
     result = x.get('drugGroup').get('conceptGroup')
     
     final_name = result[2]['conceptProperties'][0]['name']
+    
     Label1 = Label(root, text='The searched medicine is : ' + final_name)
     Label1.grid(row=5, column=0)
 
@@ -36,10 +39,19 @@ def Add_To_Prescription():
     
     final_name = result[2]['conceptProperties'][0]['name']
     prescription_list.append(final_name)
-    print(prescription_list)
 
     label2 = Label(root, text='Updated prescription list is: ' + str(prescription_list))
     label2.grid(row=6, column=0)
+
+def get_drug_from_rxcui():
+
+    x = instance.get_drug_list(string)
+    result = x.get('drugGroup').get('conceptGroup')
+    
+    final_name = result[2]['conceptProperties']
+    y = instance.fetch_drug_from_rxcui(final_name, str(final_name[0]))
+    label3 = Label(root, text=y)
+    label3.grid(row=10, column=0)
 
 
 search_button = Button(root, text='Search', command=Search_Click)
@@ -50,6 +62,10 @@ search_button.grid(row=2, column=0)
 add_to_prescription = Button(root, text='Add to Prescription', command=Add_To_Prescription)
 add_to_prescription.config(width=40)
 add_to_prescription.grid(row=3, column=0)
+
+get_drug_from_rxcui = Button(root, text = 'Search rxcui id', command = get_drug_from_rxcui)
+get_drug_from_rxcui.config(width=40)
+get_drug_from_rxcui.grid(row=4, column=0)
 
 
 root.mainloop()
